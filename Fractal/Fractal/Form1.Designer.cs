be12d5f0ab64eec1c39a934ed8fb5ae02a1491b9
alexkotov13 <alex.kotov.13.10@gmail.com>
2017-09-18 18:56:57 +0300

    Add files via upload

---
+++ Fractal/Fractal/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(32, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw fractal of Julia";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(260, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(788, 641);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(32, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Draw fractal of Square";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(32, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 70);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Draw fractal of Star Durer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(32, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(199, 70);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Draw fractal of Levi Island";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1068, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filToolStripMenuItem
+            // 
+            this.filToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.filToolStripMenuItem.Name = "filToolStripMenuItem";
+            this.filToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.filToolStripMenuItem.Text = "File";
+            this.filToolStripMenuItem.Click += new System.EventHandler(this.filToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(45, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1068, 693);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Fractal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Fractal/Fractal/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        double pi = 3.14;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //функция зарисовки фрактала
+        public void DrawFractalOfJulia(int w, int h, Graphics g, Pen pen)
+        {
+            // при каждой итерации, вычисляется znew = zold² + С
+
+            // вещественная  и мнимая части постоянной C
+            double cRe, cIm;
+            // вещественная и мнимая части старой и новой
+            double newRe, newIm, oldRe, oldIm;
+            // Можно увеличивать и изменять положение
+            double zoom = 1, moveX = 0, moveY = 0;
+            //Определяем после какого числа итераций функция должна прекратить свою работу
+            int maxIterations = 300;
+
+            //выбираем несколько значений константы С, это определяет форму фрактала Жюлиа
+            cRe = -0.70176;
+            cIm = -0.3842;
+
+            //"перебираем" каждый пиксель
+            for (int x = 0; x < w; x++)
+                for (int y = 0; y < h; y++)
+                {
+                    //вычисляется реальная и мнимая части числа z
+                    //на основе расположения пикселей,масштабирования и значения позиции
+                    newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
+                    newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
+
+                    //i представляет собой число итераций 
+                    int i;
+
+                    //начинается процесс итерации
+                    for (i = 0; i < maxIterations; i++)
+                    {
+                        //Запоминаем значение предыдущей итерации
+                        oldRe = newRe;
+                        oldIm = newIm;
+
+                        // в текущей итерации вычисляются действительная и мнимая части 
+                        newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+                        newIm = 2 * oldRe * oldIm + cIm;
+
+                        // если точка находится вне круга с радиусом 2 - прерываемся
+                        if ((newRe * newRe + newIm * newIm) > 4) break;
+                    }
+
+                    //определяем цвета
+                    pen.Color = Color.FromArgb(255, (i * 9) % 255, 0, (i * 9) % 255);
+                    //рисуем пиксель
+                    g.DrawRectangle(pen, x, y, 1, 1);
+                }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Выбираем перо "myPen" черного цвета Black
+            //толщиной в 1 пиксель:
+            Pen myPen = new Pen(Color.Black, 1);
+            //Объявляем объект "g" класса Graphics и предоставляем
+            //ему возможность рисования на pictureBox1:
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //вызываем функцию рисования фрактала
+            DrawFractalOfJulia(pictureBox1.Width, pictureBox1.Height, g, myPen);
+        }
+
+
+        double l, a; //Переменные используемые для построения каждого квадрата
+        const int d0 = 283;
+        void DrawFractalOfSquare(double c, double b) //функция построения и отрисовки квадрата
+            {
+            int i; //Переменная используемая в цикле, обозначает кол-во проходов
+            
+            const int x0 = 200, y0 = 200; //Константа, влияющая на расстояние от
+                                                    //центра квадрата до углов(Не диагональ),
+                                                    //подобрана экспериментальным путем*/
+                                                    //Координаты центра квадрата
+
+            int x1, x2, x3, x4, y1, y2, y3, y4; //Координаты углов каждого квадрата
+                Pen myPen = new Pen(Color.Black, 1); //Выбираем перо "myPen" черного цвета Black
+                                                     //толщиной в 1 пиксель
+       
+                x1 = (int)(x0 + c * Math.Cos(b + 1 * Math.PI / 4));//Координаты правой нижней
+                y1 = (int)(y0 + c * Math.Sin(b + 1 * Math.PI / 4));//точки
+                x2 = (int)(x0 + c * Math.Cos(b + 3 * Math.PI / 4)); //Координаты левой нижней 
+                y2 = (int)(y0 + c * Math.Sin(b + 3 * Math.PI / 4)); //точки
+                x3 = (int)(x0 + c * Math.Cos(b + 5 * Math.PI / 4)); //Координаты левой верхней
+                y3 = (int)(y0 + c * Math.Sin(b + 5 * Math.PI / 4)); //точки 
+                x4 = (int)(x0 + c * Math.Cos(b + 7 * Math.PI / 4));//Координаты правой верхней
+                y4 = (int)(y0 + c * Math.Sin(b + 7 * Math.PI / 4)); //точки
+                                                                    //Метод строящий фигуру по 4 точкам
+                g.DrawPolygon(myPen, new Point[] {
+                new Point(x1,y1),new Point(x2,y2),
+                new Point(x3,y3),new Point(x4,y4),
+            });
+        }
+        //Функция  построения квадратов  с заданными параметрами по нажатию кнопки 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            l = d0;
+            a = 0;
+            for (int i = 1; i <= 30; i++)//Цикл, рисующий 30 квадратов
+            {
+                DrawFractalOfSquare(l, a); //Вызов функции построения и отрисовки квадрата
+                a = a + Math.PI / 19;//изменяет угол поворота следующих квадратов
+                l = l * Math.Sin(Math.PI / 3);//изменяет размер следующих квадратов
+            }
+        }                   
+
+        void Draw_Pentagon(double x, double y, double r, double angle)
+        {            
+            Pen p;            
+            Graphics gr;
+            p = new Pen(Color.Orange, 2);
+            gr = pictureBox1.CreateGraphics();
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int i;
+            double[] x1 = new double[5];
+            double[] y1 = new double[5];
+
+            for (i = 0; i < 5; i++)
+            {
+                x1[i] = r * Math.Cos(angle + i * pi * 2 / 5);
+                y1[i] = r * Math.Sin(angle + i * pi * 2 / 5);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                gr.DrawLine(p, (int)Math.Round(x + x1[i]), (int)Math.Round(y + y1[i]), (int)Math.Round(x + x1[i + 1]), (int)Math.Round(y + y1[i + 1]));
+            }
+        }
+
+        void Draw_Star(double x, double y, double r, double angle, int d)
+        {
+            int i;
+            double h;
+
+            h = 2 * r * Math.Cos(pi / 5);
+
+            for (i = 0; i < 5; i++)
+            {
+                Draw_Pentagon(x - h * Math.Cos(angle + i * pi * 2 / 5), y - h * Math.Sin(angle + i * pi * 2 / 5), r, angle + pi + i * pi * 2 / 5);
+
+                if (d > 0)
+                    Draw_Star(x - h * Math.Cos(angle + i * pi * 2 / 5), y - h * Math.Sin(angle + i * pi * 2 / 5), r / (2 * Math.Cos(pi / 5) + 1), angle + pi + (2 * i + 1) * pi * 2 / 10, d - 1);
+            }
+            Draw_Pentagon(x, y, r, angle);
+
+            if (d > 0)
+                Draw_Star(x, y, r / (2 * Math.Cos(pi / 5) + 1), angle + pi, d - 1);
+        }     
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SolidBrush fon;
+            fon = new SolidBrush(Color.Black);
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Draw_Star(300, 300, 85, pi / 2, 4);
+        }
+
+   
+        void Draw_Levy(Graphics gr, Pen p, SolidBrush fon, int x1, int x2, int y1, int y2, int i)
+        {           
+            if (i == 0)
+            {
+                gr.DrawLine(p, x1, y1, x2, y2);
+            }
+            else
+            {
+                int x3 = (x1 + x2) / 2 + (y2 - y1) / 2;
+                int y3 = (y1 + y2) / 2 - (x2 - x1) / 2;
+
+                Draw_Levy(gr, p, fon, x1, x3, y1, y3, i - 1);
+                Draw_Levy(gr, p, fon, x3, x2, y3, y2, i - 1);
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Pen p = new Pen(Color.Lime, 2);
+            SolidBrush fon = new SolidBrush(Color.Black);
+            Graphics gr = pictureBox1.CreateGraphics();
+            int i = 15;
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Draw_Levy(gr, p, fon, 250, 400, 160, 160, i); // Строим кривую Леви
+            Draw_Levy(gr, p, fon, 400, 400, 160, 310, i); // на каждой
+            Draw_Levy(gr, p, fon, 400, 250, 310, 310, i); // стороне 
+            Draw_Levy(gr, p, fon, 250, 250, 310, 160, i); // квадрата
+        }
+
+        private void filToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g = null;
+            pictureBox1.Image = null;
+            pictureBox1.Update();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {          
+            saveFileDialog1.DefaultExt = "png";
+            saveFileDialog1.Filter = "Image files (.png)|*png";
+            saveFileDialog1.ShowDialog();                             
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height, g);
+            bmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+
+            try
+            {
+                MessageBox.Show("Файл сохранен!", "Удачное сохранение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //source = Image.FromFile(fileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось сохранить файл: " + ex.Message);
+            }
+        }
+
+
+    }
+}
